46faca5207a49b1839fea9bf505c88839011a390
Miranda-Zeng <hz2961@nyu.edu>
2023-04-20 14:59:36 +0800

    add speed on x-axis

---
+++ vanilla-js-and-p5/sketch.js
@@ -0,0 +1,28 @@
+let x=200;
+let xSpeed = 1;
+
+let FasterButton = document.getElementById("fast");
+
+
+function setup(){
+    let cnv = createCanvas(400,400);
+    cnv.parent("canvasContainer");
+  
+}
+
+function draw(){
+    background(120,40,240);
+  noStroke();
+  
+ circle(x,height/2,50);
+x += xSpeed;
+if(x>width){
+    x=0;
+}
+
+}
+function increaseSpeed(){
+    xSpeed ++;
+}
+
+FasterButton.addEventListener("click",increaseSpeed);
