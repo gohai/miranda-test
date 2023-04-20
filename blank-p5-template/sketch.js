460e2d268aa51b26d576d7b8ca3848509d1c5796
Miranda-Zeng <hz2961@nyu.edu>
2023-04-20 14:02:08 +0800

    Initial commit

---
+++ blank-p5-template/sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+    let cnv = createCanvas(400,400);
+    cnv.parent("canvasContainer");
+  
+}
+
+function draw(){
+    background(120,40,240);
+  noStroke();
+  
+ circle(width/2,height/2,50);
+
+}
---
+++ bring-js-back/script.js
@@ -0,0 +1,16 @@
+alert("hiiiiii");
+window.open("",)
+
+let myValue = 2;
+myValue =+ 1;
+console.log(myValue);
+
+
+//select element on oage
+let headline = document.getElementById("");
+let theButton = document.getElementById("");
+
+//manipulate the selected element
+headline.content = ""
+
+//attach event
